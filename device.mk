f8e760135554c6c8c1f39b99f8b2281359e013e5
Jurrwhy <jurrwhy@gmail.com>
2026-01-11 13:53:13 -0600

    Marble: PixelOS time

--- device.mk
+++ device.mk
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \
     NfcResMarble \
     SettingsProviderResMarble \
     SettingsProviderResMarbleCN \
-    SettingsResMarble \
     SystemUIResMarble \
     WifiResMarble \
     WifiResMarbleCN \
